c9a938cf5092b5661c3ab9d59525c000da3b775a
Enbao Cao <ecao25@sjs.org>
2024-03-29 21:38:04 -0500

    about page descriptions and footer

--- src/About.js
+++ src/About.js
@@ -13,34 +13,47 @@ function About() {
 
 return (
     <div className="webpage">
-        <section className="hero-section">
-            <div className="hero-content">
-                <h1 className = "Title">About Us</h1>
-                <p>Welcome to the Problem Solving Initiative (PSI), where homeschooling meets zero-cost interactive mathematics education! At PSI, we are dedicated to providing a dynamic online learning environment designed to enhance critical thinking skills in mathematics for homeschooled children.</p>
+        <section className="section no-highlight">
+            <h1 className = "Title">About Us</h1>
+            <p>Welcome to the Problem Solving Initiative (PSI), where homeschooling meets zero-cost interactive mathematics education! At PSI, we are dedicated to providing a dynamic online learning environment designed to enhance critical thinking skills in mathematics for homeschooled children.</p>
+        </section>
+
+        <section className="section no-highlight">
+            <h2>Our Mission</h2>
+            <p>Fostering a love in mathematics requires a deep understanding of how things work together. With a database of thousands of hand-curated word problems selected to hone critical thinking in applicable real-world circumstances, we hope to bring mathematics to life for your child. </p>
+        </section>
+
+        <section className="section">
+            <h2>Features</h2>
+            <div class="features no-highlight">
+                <div class="feature">
+                    <div class="feature-title">Lightweight User Interface</div>
+                    <p>Enjoy a user-friendly, intuitive interface designed for easy navigation and seamless user experience.</p>
+                </div>
+                <div class="feature">
+                    <div class="feature-title">Progress Tracking</div>
+                    <p>Keep track of previously completed problems via the problem list under the Learn! platform and monitor your performance over time.</p>
+                </div>
+                <div class="feature">
+                    <div class="feature-title">Step-by-Step Solutions</div>
+                    <p>Receive comprehensive step-by-step solutions for each problem to really understand the underlying concepts and reasoning behind each answer.</p>
+                </div>
+                <div class="feature">
+                    <div class="feature-title">Personal Accounts</div>
+                    <p>Create personalized accounts to track your progress and mastery level through experience points, encouraging continuous improvement.</p>
+                </div>
+                <div class="feature">
+                    <div class="feature-title">Multi-platform Compatibility</div>
+                    <p>Access the platform on various devices, including desktops, laptops, tablets, and smartphones.</p>
+                </div>
+                <div class="feature">
+                    <div class="feature-title">Secure and Privacy-Compliant</div>
+                    <p>We prioritize your privacy through encryption, secure authentication, and compliance with privacy regulations such as GDPR and COPPA.</p>
+                </div>
             </div>
         </section>
 
-        <section className="mission-section">
-            <div className="section-content">
-                <h2>Our Mission</h2>
-                <p>Fostering a love in mathematics requires a deep understanding of how things work together. With a database of thousands of hand-curated word problems selected to hone critical thinking in applicable real-world circumstances, we hope to bring mathematics to life for your child. </p>
-            </div>
-        </section>
-
-        <section className="mission-section">
-            <div className="section-content">
-                <h2>Features</h2>
-                <p><strong>Lightweight User Interface:</strong> Enjoy a user-friendly and intuitive interface designed for easy navigation and seamless user experience across devices.</p>
-                <p><strong>Personal Accounts:</strong> Create personalized accounts to access exclusive features, track progress, and save preferences for a customized learning journey.</p>
-                <p><strong>Complete Step-by-Step Solutions:</strong> Receive comprehensive step-by-step solutions for each problem, helping users understand the underlying concepts and reasoning behind the answers.</p>
-                <p><strong>Experience Bar:</strong> Track your progress and mastery level with an experience bar that visually represents your achievements and encourages continuous improvement.</p>
-                <p><strong>Progress Tracking:</strong> Keep track of previously completed problems and monitor your performance over time, enabling targeted practice and focused improvement in areas of difficulty.</p>
-                <p><strong>Multi-platform Compatibility:</strong> Access the platform on various devices, including desktops, laptops, tablets, and smartphones, ensuring flexibility and convenience for users.</p>
-                <p><strong>Secure and Privacy-Compliant:</strong> Prioritize user privacy and data security through encryption, secure authentication methods, and compliance with relevant privacy regulations such as GDPR and COPPA.</p>
-            </div>
-        </section>
-
-        <section className="contact-section">
+        <section className="section">
                 <div className="section-content">
                     <h2>Contact Us</h2>
                     {isHuman ? (
@@ -58,7 +71,12 @@ return (
                         </>
                     )}
                 </div>
-            </section>
+        </section>
+
+        <div className="footer">
+            Made with ❤️ from Houston, Texas
+        </div>
+
     </div>
 
 );
